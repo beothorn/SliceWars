49ab2727384c8f44d939a0ecea4fe8ae86a9757d
Lucas <lucass@gmail.com>
2013-03-23 14:48:50 -0300

    Showing a list of contacts to choose for playing

--- src/main/java/skype/SkypeClient.java
+++ src/main/java/skype/SkypeClient.java
@@ -1,10 +1,22 @@
 package skype;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
 
-import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+
+import org.apache.commons.lang.UnhandledException;
 
 import sliceWars.RemoteInvite;
 import sliceWars.gui.GuiPlayer;
@@ -12,6 +24,7 @@ import sliceWars.logic.Player;
 
 import com.skype.Application;
 import com.skype.ApplicationAdapter;
+import com.skype.Friend;
 import com.skype.Skype;
 import com.skype.SkypeException;
 import com.skype.Stream;
@@ -78,7 +91,40 @@ public class SkypeClient {
             }
         });
         
-        String opponentId = JOptionPane.showInputDialog(null, "Enter player id : ", "gabrielsan", 1);
-        return application.connect(opponentId);
+        Friend[] allFriends = Skype.getContactList().getAllFriends();
+        List<String> contacts = new ArrayList<String>();
+        for (Friend friend : allFriends) {
+        	contacts.add(friend.getId());
+		}
+        
+        JFrame jFrame = new JFrame();
+        jFrame.setLayout(new BorderLayout());
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		final JList<String> contactList = new JList(contacts.toArray());
+		contactList.setSize(100, 800);
+		JScrollPane jScrollPane = new JScrollPane(contactList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		
+		jFrame.add(jScrollPane, BorderLayout.CENTER);
+		Button button = new Button("Play");
+		final AtomicReference<String> opponentId = new AtomicReference<String>();
+		final CountDownLatch countDownLatch = new CountDownLatch(1);
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				opponentId.set(contactList.getSelectedValue());
+				countDownLatch.countDown();
+			}
+		});
+		jFrame.add(button, BorderLayout.SOUTH);
+		jFrame.pack();
+        jFrame.setVisible(true);
+        
+        try {
+			countDownLatch.await();
+		} catch (InterruptedException e1) {
+			throw new UnhandledException(e1);
+		}
+        String oponnent = opponentId.get();
+		return application.connect(oponnent);
     }
 }
