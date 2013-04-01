08d3e51be7965035838ed71ebcdc4e5836e16060
Lucas <lucass@gmail.com>
2013-04-01 12:55:07 -0300

    Select at least one contact

--- src/main/java/skype/SkypeMain.java
+++ src/main/java/skype/SkypeMain.java
@@ -2,6 +2,8 @@ package skype;
 
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 import sliceWars.remote.ContactListFrame;
 import sliceWars.remote.ContactListFrame.ContactListSelected;
 
@@ -19,6 +21,10 @@ public class SkypeMain {
 		final SkypeBridge skypeBridge = new SkypeBridge(SkypeApp.getOwnId());
         
         contactListFrame.setContactListSelectedListener(new ContactListSelected(){@Override public void selected(List<String> selectedValuesList) {
+        	if(selectedValuesList.size() == 0){
+        		JOptionPane.showMessageDialog(null,"Select at least one contact.");
+        		return;
+        	}
         	contactListFrame.close();
         	skypeBridge.setServer();
         	skypeBridge.setInvited(selectedValuesList);
